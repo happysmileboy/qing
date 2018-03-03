c6df6f4d3f8d20bed383a0a1c83be739b9d4339c
happysmileboy <t3511410@naver.com>
2018-03-03 15:18:16 +0900

    d

--- qing/settings/prod.py
+++ qing/settings/prod.py
@@ -1,3 +1,14 @@
 from .common import *
 
 ALLOWED_HOSTS = ['18.220.12.176', ]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'qing',
+        'USER': 'rocketdan',
+        'PASSWORD': 'rocketdan',
+    },
+}
