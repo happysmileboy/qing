61f356e5724d8284c63f4306f561643ca1ccea6c
happysmileboy <t3511410@naver.com>
2018-03-19 15:11:26 +0900

    랜덤함수생성

--- accounts/utils.py
+++ accounts/utils.py
@@ -5,9 +5,9 @@ import random
 def generate_random_string(length=60):
     choice_set = string.ascii_letters + string.digits
     num =1
-    random_string =''
+    random_string = ''
     while num <=20:
         num += 1
-        random_string.join(random.choice(choice_set))
+        random_string += random.choice(choice_set)
 
     return random_string
